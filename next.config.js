4c4c0d3c89147d6dd983251d502c1c38c6965a96
SeifMohy <seifmohy@gmail.com>
2025-05-11 18:00:38 +0300

    build fix 2

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['chart.js', 'react-chartjs-2'],
+  transpilePackages: ['chart.js', 'react-chartjs-2', 'styled-components'],
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
     esmExternals: 'loose'
+  },
+  compiler: {
+    styledComponents: true
   }
 }
 
